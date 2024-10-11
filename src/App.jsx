5d3ef60f81c97206d8f79a281625ab2fa4afd04a
Dzmitry <dimaau1@gmail.com>
2024-10-11 14:21:38 +0300

    weather app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+import './App.css';
+import Weather from './components/Weather';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Weather />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
