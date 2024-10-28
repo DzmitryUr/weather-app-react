5ddc2ea234d670e0fb3370879b11ef6bb1ff39fa
Dzmitry <dimaau1@gmail.com>
2024-10-28 15:06:46 +0200

    add switcher

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
+import { useState, useEffect } from 'react';
+
 export function Header() {
-  const toggleTheme = () => {
-    document.documentElement.classList.toggle('dark');
-  };
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    darkTheme ? root.classList.add('dark') : root.classList.remove('dark');
+  }, [darkTheme]);
 
   return (
     <header className='flex justify-between items-center bg-white dark:bg-gray-800 dark:text-white shadow-md rounded-lg mb-4 w-full p-2'>
       <h2 className='text-lg font-bold p-2'>React Weather App</h2>
       <button
-        onClick={toggleTheme}
+        onClick={() => setDarkTheme(!darkTheme)}
         className='p-2 bg-blue-500 text-white rounded'
       >
-        Toggle Mode
+        {darkTheme ? 'Light' : 'Dark'}
       </button>
     </header>
   );
